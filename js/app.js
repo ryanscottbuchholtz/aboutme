19639a2e82add5e3a48559554c9ca1b09a747b5a
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-04-19 12:38:48 -0700

    js work

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 function sticky() {
   var window_top = $(window).scrollTop();
   var div_top = $('#sticky-anchor').offset().top;
-  console.log(window_top);
-  console.log(div_top);
+  console.log(window_top + '-' +div_top); 
   if (window_top > div_top) {
     $('#language').addClass('stick');
   } else {
